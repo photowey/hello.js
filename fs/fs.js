f1bcdb2e993c1b14dd1e4d4e377718de756aedf7
photowey <photowey@gmail.com>
2023-03-16 00:24:21 +0800

    feat: 1.Update: description

--- fs/fs.js
+++ fs/fs.js
@@ -4,6 +4,6 @@
  * @LastEditTime: 2023/03/15 10:09:19
  * @LastEditors: photowey
  * @Description: NodeJS fs module
- * @FilePath: /hello.js/fz/fs.js
+ * @FilePath: /hello.js/fs/fs.js
  * @Copyright (c) 2023 by photowey<photowey@gmail.com> All Rights Reserved
  */
--- path/path.js
+++ path/path.js
@@ -1,9 +1,9 @@
 /**
  * @Author: photowey
  * @Date: 2023/03/15 10:09:28
- * @LastEditTime: 2023/03/16 00:10:56
+ * @LastEditTime: 2023/03/16 00:23:42
  * @LastEditors: photowey
  * @Description: NodeJS path module
- * @FilePath: /hello.js/pathz/path.js
+ * @FilePath: /hello.js/path/path.js
  * @Copyright (c) 2023 by photowey<photowey@gmail.com> All Rights Reserved
  */
