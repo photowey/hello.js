8678e92a66d8d54896c360d2a6cde611dc799e14
photowey <photowey@gmail.com>
2023-03-15 10:06:50 +0800

    feat: 1.Add simple function invoke

--- hello/hello.js
+++ hello/hello.js
@@ -1,3 +1,25 @@
+/**
+ * @Author: photowey
+ * @Date: 2023/03/15 09:39:48
+ * @LastEditTime: 2023/03/15 10:06:11
+ * @LastEditors: photowey
+ * @Description: NodeJS hello project
+ * @FilePath: /hello.js/hello/hello.js
+ * @Copyright (c) 2023 by photowey/photowey@gmail.com All Rights Reserved
+ */
 console.log('Hello World~')
 
+// Global object
 console.log(globalThis === global)
+
+// function invoke
+sayHello('djungo')
+
+/** 
+ * @description: say hello to someone.
+ * @param {string} name the target of say hello.
+ * @return {void}
+ */
+function sayHello(name) {
+    console.log('Hello %s~', name)
+}
